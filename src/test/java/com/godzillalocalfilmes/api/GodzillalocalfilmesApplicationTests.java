0ace19d0be143a9e6e570abc132ae64db881f60e
Pedro1840 <pedrocontato1840@gmail.com>
2024-09-25 21:49:38 -0300

    first commit

---
+++ src/test/java/com/godzillalocalfilmes/api/GodzillalocalfilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godzillalocalfilmes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodzillalocalfilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
